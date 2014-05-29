019e63981f753d9f2f9bf7441f278debd707e1d7
Tony Castronova <castronova.anthony@gmail.com>
2014-05-29 16:55:59 -0600

    Added some test models.

---
+++ tests/data/multiplier.py
@@ -0,0 +1,6 @@
+__author__ = 'tonycastronova'
+
+class multiplier(object):
+    def __init__(self):
+        pass
+
---
+++ tests/data/random.py
@@ -0,0 +1,9 @@
+__author__ = 'tonycastronova'
+
+import random
+
+class randomizer(object):
+    def __init__(self):
+        self.__rand = random.random()
+
+
